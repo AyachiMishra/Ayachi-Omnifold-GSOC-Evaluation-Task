8ff99a4ad6ff1f7603c4b5ad2256e3270d8d0718
Ayachi Padmanabh Mishra <ayachipadmanabh@gmail.com>
2026-04-03 10:28:31 +0400

    chore: removing folder complexity

---
+++ weekly_progress/week1_metadata/validate_metadata.py
@@ -0,0 +1,5 @@
+"""
+This file will compare the yaml metadata files against the proposed schema using jsonschema library method named validate.
+
+This file will again be used when we create the final validate_metadata file...
+"""
